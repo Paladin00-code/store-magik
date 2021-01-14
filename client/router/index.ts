3943ccd0d53b02cad0d79b5ce617cb3b65f592a1
harshperson <harshperson@gmail.com>
2021-01-14 11:36:33 +0200

    add components to client

---
+++ client/router/index.ts
@@ -0,0 +1,35 @@
+// import { , createWebHistory, RouteRecordRaw } from 'vue-router'
+import createRouter from 'vue-router'
+import createWebHistory from 'vue-router'
+import RouteRecordRaw from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../components/Home.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import('../components/Signup.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../components/Login.vue')
+  },
+  {
+    path: '/forgot-password',
+    name: 'forgot-password',
+    component: () => import('../components/ForgotPassword.vue')
+  }
+]
+
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>
-      
+  <div class="vue-tempalte">
+    <!-- Navigation -->
+    <nav class="navbar shadow bg-dark rounded justify-content-between flex-nowrap flex-row fixed-top">
+      <div class="container">
+        <a class="navbar-brand float-left" href="/">
+          Home
+        </a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="btn btn-outline-primary" to="/signup">Sign up</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Main -->
+    <div class="App">
+      <div class="vertical-center">
+        <div class="inner-block">
+          <router-view />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
-
-<script>
-
-
-export default {
-  name: 'App',
-  components: {
-    
-  }
-}
-</script>
